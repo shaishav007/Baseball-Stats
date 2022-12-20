554ae7fe8a7e2c1666bb5199496ba20945063fbd
shaishav007 <shaishav007@gmail.com>
2022-12-20 10:45:13 -0500

    everything works

--- homebase/urls.py
+++ homebase/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('overview/',views.displayOverview),
     path('overview/team-<int:teamId>/hitters/',views.loadTeamView),
+    path('overview/team-<int:teamId>/hitters/player-<int:playerId>/',views.YearlyStatsView),
+    path('overview/team-<int:teamId>/pitchers/player-<int:playerId>/',views.YearlyStatsView),
     path('overview/team-<int:teamId>/pitchers/',views.loadTeamView),
     path('overview/team-<int:teamId>/player-<int:playerId>/',views.YearlyStatsView),
     path('overview/team-<int:teamId>/',views.loadTeamView),
